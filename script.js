704fdd7e28e71e38ee2100f0dd05d09ade50efae
Hernando Abella <hernandoabella@gmail.com>
2021-12-14 22:14:55 -0500

    algunos cambios en el dise;o

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // contador
 
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+	counter.innerText = "0";
+
+	const updateCounter = () => {
+		const target = +counter.getAttribute("data-target");
+		const count = +counter.innerText;
+
+		const increment = target / 200;
+
+		if (count < target) {
+			counter.innerText = `${Math.ceil(count + increment)}`;
+			setTimeout(updateCounter, 10);
+		} else {
+			counter.innerText = target;
+		}
+	};
+	updateCounter();
+});
